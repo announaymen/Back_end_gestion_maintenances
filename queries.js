b0fe99b17e5aa78778eeeb81f216c76b3ab37d50
announaymen <a.announ@esi-sba.dz>
2020-06-18 00:06:42 +0100

    Update queries.js

--- queries.js
+++ queries.js
@@ -113,7 +113,7 @@ const auth = (request, response) => {
         response.status(200).send(error);
       }
       if (results.rowCount < 1) {
-        response.setHeader("custom_header_name", "abcde");
+        response.writeHead(200, { "Content-Type": "text/event-stream" });
         response.status(200).send("authentification feild");
       } else response.status(200).send("signed-in!!");
     }
