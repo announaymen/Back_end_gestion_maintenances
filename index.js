9b33912529e896b79d850dc1b1185519db795a82
announaymen <a.announ@esi-sba.dz>
2020-07-07 14:45:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header(
-    ("Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS")
+    ("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
   );
 
   next();
